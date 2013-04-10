770177bbdae869a0e45eebb0e36cac02bf193af9
David Alexander <dalexander@pacificbiosciences.com>
2013-04-10 10:55:47 -0800

    New bash5tools.py implementation, using BasH5Reader
    A few notes:
    
      * Only FASTA and FASTQ output are currently implemented.  CSV can
        come later if we really need it.
    
      * --readType={ccs, unrolled, subreads} to choose what you want
    
      * Command-line arguments now follow a sane camel-case convention
    
      * Performance is better than the previous implementation but there
        is still a lot of room for improvement if needed.  For example, I
        would like to eliminate the binary searching of the region
        table at some point.
    
    [Tracking bug 22690]
    
    [git-p4: depot-paths = "//depot/software/bioinformatics/tools/pbh5tools/": change = 122452]

--- src/python/bash5tools.py
+++ src/python/bash5tools.py
@@ -1,105 +1,121 @@
 #!/usr/bin/env python
 
-#################################################################################$$
-# Copyright (c) 2011,2012, Pacific Biosciences of California, Inc.
-#
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
-# * Redistributions of source code must retain the above copyright notice, this 
-#   list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice, 
-#   this list of conditions and the following disclaimer in the documentation 
-#   and/or other materials provided with the distribution.
-# * Neither the name of Pacific Biosciences nor the names of its contributors 
-#   may be used to endorse or promote products derived from this software 
-#   without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY PACIFIC BIOSCIENCES AND ITS CONTRIBUTORS 
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
-# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PACIFIC BIOSCIENCES OR ITS 
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#################################################################################$$
+import os, os.path, sys, argparse, logging
 
-import os
-import sys
-import argparse
-import logging
-import pkg_resources
-
-from pbtools.pbh5tools.BasH5ToData import BASH5Data
 from pbcore.util.ToolRunner import PBToolRunner
+from pbcore.io import (BasH5Reader,
+                       FastaWriter,
+                       FastqWriter)
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
+def _fileType(arg):
+    """
+    Canonicalize the given filetype argument
+    """
+    if   arg in ["fa", "fasta", "FASTA"]: return "fasta"
+    elif arg in ["fq", "fastq", "FASTQ"]: return "fastq"
+    elif arg in ["csv"]:                  return "csv"
+    else: raise ValueError("Unsupported output file format")
+
+
+class FastaEmitter(object):
+    def __init__(self, filename):
+        self.writer = FastaWriter(filename)
+
+    def emit(self, zmwRead):
+        self.writer.writeRecord(zmwRead.readName,
+                                zmwRead.basecalls())
+
+class FastqEmitter(object):
+    def __init__(self, filename):
+        self.writer = FastqWriter(filename)
+
+    def emit(self, zmwRead):
+        self.writer.writeRecord(zmwRead.readName,
+                                zmwRead.basecalls(),
+                                zmwRead.QualityValue())
 
 class BasH5ToolsRunner(PBToolRunner):
-    
+
     def __init__(self):
-        desc = ['Tool for extracting data from .bas.h5',
-                'Notes: For all command-line arguments, default values are listed in [].']
-        super(BasH5ToolsRunner, self).__init__('\n'.join(desc))
+        desc = "Tool for extracting data from .bas.h5 files"
+        super(BasH5ToolsRunner, self).__init__(desc)
 
-        self.parser.add_argument('infile', metavar='input.bas.h5',
-                               help='input .bas.h5 filename')
-        self.parser.add_argument('--outFilePref', dest='outfilepref', default='', 
-                               help='output filename prefix [%(default)s]')
-        self.parser.add_argument('--readType', dest='readtype', default='Raw', 
-                               help='read type (CCS or Raw) [%(default)s]')
-        self.parser.add_argument('--outType', dest='outtype', default='fasta', 
-                               help='output file type (fasta, fastq, csv or detail) [%(default)s]')
-        self.parser.add_argument('--subReads', dest='subreads', action='store_true', 
-                               help='for fasta/fastq files, generate the subreads and not the raw reads [%(default)s]')
+        self.parser.add_argument(
+            "inFile", metavar="input.bas.h5",
+            help="input .bas.h5 filename")
+        self.parser.add_argument(
+            "--outFilePrefix", dest="outFilePrefix", default=None,
+            help="output filename prefix [%(default)s]")
+        self.parser.add_argument(
+            "--readType", dest="readType", default="subreads",
+            choices=["ccs", "subreads", "unrolled"],
+            help="read type (ccs, subreads, or unrolled) [%(default)s]")
+        self.parser.add_argument(
+            "--outType", dest="outType", default="fasta", type=_fileType,
+            help="output file type (fasta, fastq, or csv) [%(default)s]")
 
-        groupFilt = self.parser.add_argument_group('read filtering arguments')
-        groupFilt.add_argument('--minLength', type=int, dest='minlen', default=0,
-                               help='min read length [%(default)s]')
-        groupFilt.add_argument('--minReadscore', type=float, dest='minrs', default=0,
-                               help='min read score, valid only when used with --readType=Raw [%(default)s]')
-        groupFilt.add_argument('--minMeanQV', type=int, dest='minmqv', default=0,
-                               help='min per read mean Quality Value [%(default)s]')
-        groupFilt.add_argument('--minNumberOfPasses', type=int, dest='minnp', default=0,
-                               help='min number of CCS passes, valid only when used with --readType=CCS [%(default)s]')
+        groupFilt = self.parser.add_argument_group("Read filtering arguments")
+        groupFilt.add_argument(
+            "--minLength", type=int, dest="minLength", default=0,
+            help="min read length [%(default)s]")
+        groupFilt.add_argument(
+            "--minReadScore", type=float, dest="minReadScore", default=0,
+            help="min read score, valid only with --readType={unrolled,subreads}  [%(default)s]")
+        groupFilt.add_argument(
+            "--minPasses", type=int, dest="minPasses", default=0,
+            help="min number of CCS passes, valid only with --readType=ccs [%(default)s]")
+
     def getVersion(self):
         return __version__
 
     def validateArgs(self):
-        if not os.path.isfile(self.args.infile):
-            self.parser.error('File %s does not exist!' % self.args.infile)
-        if self.args.readtype not in ['CCS', 'Raw']:
-            self.parser.error('%s is not a valud read type, only CCS and Raw read types are supported!' % self.args.readtype)            
-        if self.args.outtype not in ['fasta', 'fastq', 'csv', 'detail']:
-            self.parser.error('%s is not a valid file type, only fasta, fastq, csv and detail files are supported!' %
-                                 self.args.outtype)                    
-        if self.args.minrs > 1.0 or self.args.minrs < 0.0:
-            self.parser.error('Minimum readscore needs to be > 0.0 and < 1.0')
+        if not os.path.isfile(self.args.inFile):
+            self.parser.error("File %s does not exist!" % self.args.infile)
+        if self.args.minReadScore > 1.0 or self.args.minReadScore < 0.0:
+            self.parser.error("Minimum read score needs to be > 0.0 and < 1.0")
+
+    def zmwReads(self, inBasH5, readType):
+        """
+        Extract all reads of the appropriate read type
+        """
+        for zmw in inBasH5:
+            if readType == "ccs":
+                r = zmw.ccsRead
+                if r: yield r
+            elif readType == "unrolled":
+                yield zmw.read()
+            else:
+                for r in zmw.subreads:
+                    yield r
 
     def run(self):
-        filters = {'ReadScore': self.args.minrs,
-                   'ReadLength': self.args.minlen,
-                   'CCSCov': self.args.minnp,
-                   'MinQV': self.args.minmqv}
-        map(lambda x: filters.pop(x) if not filters[x] else None, filters.keys())
+        inBasH5 = BasH5Reader(self.args.inFile)
+        movieName = inBasH5.movieName
+        outFilePrefix = self.args.outFilePrefix or movieName
+        outFilename = "%s.%s" % (outFilePrefix, self.args.outType)
 
-        logging.info('Loading .bas.h5 file from %s' % self.args.infile)                
-        bash5 = BASH5Data(self.args.infile, readType=self.args.readtype, filters=filters)
+        if self.args.outType == "fasta":
+            sink = FastaEmitter(outFilename)
+        elif self.args.outType == "fastq":
+            sink = FastqEmitter(outFilename)
+        else:
+            print "CSV output support not yet implemented!"
+            sys.exit(-1)
 
-        logging.info('Generating %s file' % self.args.outtype)                
-        if self.args.outtype in ['fasta','fastq']:
-            bash5.toFastx(outFNPref=self.args.outfilepref, fileType=self.args.outtype, subreadsplit=self.args.subreads)
-        elif self.args.outtype == 'csv':
-            bash5.toCSV(outFNPref=self.args.outfilepref)
-        elif self.args.outtype == 'detail':
-            bash5.toDetail(outFNPref=self.args.outfilepref)
+        readType = self.args.readType
+        for zmwRead in self.zmwReads(inBasH5, self.args.readType):
+            zmw = zmwRead.zmw
+            #
+            # Emit read if filters pass
+            #
+            if ((readType != "ccs" or zmw.numPasses >= self.args.minPasses)    and
+                (readType == "ccs" or zmw.readScore >= self.args.minReadScore) and
+                (len(zmwRead) >= self.args.minLength)):
 
-if __name__ == '__main__':    
+                sink.emit(zmwRead)
+
+
+if __name__ == "__main__":
     sys.exit(BasH5ToolsRunner().start())
-
